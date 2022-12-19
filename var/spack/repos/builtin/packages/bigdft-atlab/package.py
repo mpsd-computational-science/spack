d20f2841009eb037344e73516d38716769643e4b
Daniele Cesarini <d.cesarini@cineca.it>
2022-12-19 21:18:18 +0100

    Fixed several bugs

--- var/spack/repos/builtin/packages/bigdft-atlab/package.py
+++ var/spack/repos/builtin/packages/bigdft-atlab/package.py
@@ -18,6 +18,10 @@ class BigdftAtlab(AutotoolsPackage):
     version("1.9.1", sha256="3c334da26d2a201b572579fc1a7f8caad1cbf971e848a3e10d83bc4dc8c82e41")
     version("1.9.0", sha256="4500e505f5a29d213f678a91d00a10fef9dc00860ea4b3edf9280f33ed0d1ac8")
 
+    depends_on("autoconf", type="build")
+    depends_on("automake", type="build")
+    depends_on("libtool", type="build")
+
     variant("mpi", default=True, description="Enable MPI support")
     variant("openmp", default=True, description="Enable OpenMP support")
     variant("openbabel", default=False, description="Enable detection of openbabel compilation")
@@ -28,33 +32,30 @@ class BigdftAtlab(AutotoolsPackage):
     for vers in ["1.9.0", "1.9.1", "1.9.2", "develop"]:
         depends_on("bigdft-futile@{0}".format(vers), when="@{0}".format(vers))
 
-    build_directory = "atlab"
-
-    def autoreconf(self, spec, prefix):
-        autoreconf = which("autoreconf")
-
-        with working_dir(self.build_directory):
-            autoreconf("-fi")
+    configure_directory = "atlab"
 
     def configure_args(self):
         spec = self.spec
         prefix = self.prefix
 
-        openmp_flag = []
+        fcflags = []
         if "+openmp" in spec:
-            openmp_flag.append(self.compiler.openmp_flag)
+            fcflags.append(self.compiler.openmp_flag)
+
+        if self.spec.satisfies("%gcc@10:"):
+            fcflags.append("-fallow-argument-mismatch")
 
         args = [
-            "FCFLAGS=%s" % " ".join(openmp_flag),
-            "--with-futile-libs=%s" % spec["bigdft-futile"].prefix.lib,
-            "--with-futile-incs=%s" % spec["bigdft-futile"].prefix.include,
+            "FCFLAGS=%s" % " ".join(fcflags),
+            "--with-futile-libs=%s" % spec["bigdft-futile"].libs.ld_flags,
+            "--with-futile-incs=%s" % spec["bigdft-futile"].headers.include_flags,
             "--with-moduledir=%s" % prefix.include,
             "--prefix=%s" % prefix,
             "--without-etsf-io",
         ]
 
         if "+mpi" in spec:
-            args.append("CC=%s" % spec["mpi"].mpicc)
+            args.append("CC=%s " % spec["mpi"].mpicc)
             args.append("CXX=%s" % spec["mpi"].mpicxx)
             args.append("FC=%s" % spec["mpi"].mpifc)
             args.append("F90=%s" % spec["mpi"].mpifc)
